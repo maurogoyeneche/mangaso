2c7e19177be864f38073bd14c60b9a2603e9bcd2
Mauro Goyeneche <maurogoyeneche@gmail.com>
2025-07-20 15:51:02 -0300

    fix config and style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "server", // Habilitar SSR para rutas dinámicas
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   vite: {
     plugins: [tailwindcss()],
   },
